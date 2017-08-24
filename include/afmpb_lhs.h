6ceb8f0b115be61bab486eab7905e48005e3bb35
Bo Zhang <zhang416@indiana.edu>
2017-08-24 09:48:25 -0400

    update license boilerplate

--- include/afmpb_lhs.h
+++ include/afmpb_lhs.h
@@ -1,3 +1,17 @@
+//=============================================================================
+// AFMPB: Adaptive Fast Multipole Poisson-Boltzmann Solver 
+//
+// Portions Copyright (c) 2014, Institute of Computational Mathematics, CAS
+// Portions Copyright (c) 2014, Oak Ridge National Laboratory
+// Portions Copyright (c) 2017, Trustees of Indiana University,
+//
+// All rights reserved.
+//
+// This program is a free software; you can redistribute it and/or modify it
+// uner the terms of the GNU General Public License version 3 as published by 
+// the Free Software Foundation. 
+//=============================================================================
+
 #ifndef __AFMPB_LHS_H__
 #define __AFMPB_LHS_H__
 
